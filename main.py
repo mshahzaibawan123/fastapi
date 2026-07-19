46d511c8abd2eb22d9b5376bab1ca6a4cff413ff
Muhammad Shahzaib iqbal <mshahzaibawan123@gmail.com>
2026-07-19 13:50:07 +0500

    Add root and health endpoints

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def root():
+#     return {"message": "Hello! Welcome to Task API"}
+# ---------------------------------------------------------
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Task CRUD API",
+    description="A simple Task Management API built with FastAPI",
+    version="1.0.0"
+)
 
-@app.get("/")
-def root():
-    return {"message": "Hello! Welcome to Task API"}
